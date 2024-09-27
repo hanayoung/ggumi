eb041c39cd36f8c8146cfab4e8dd21dfe2e6515d
yujin <lynatic13@naver.com>
2024-09-27 17:49:31 +0900

    0927 상어초등학교 풀다가 말음

---
+++ BOJ/src/BOJ/boj_21608_상어초등학교.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_21608_상어초등학교 {
+	static int N;
+	static int room[][];
+	static int saticefy;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		room = new int[N+1][N+1];
+		
+	}
+
+}
